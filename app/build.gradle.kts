4134060ebe6f3644f110595b2b4b7bb20f80ff77
raisul1313 <raisul.islam1313@gmail.com>
2025-06-24 18:11:33 +0600

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id(BuildPlugins.ANDROID_APPLICATION)
+    id(BuildPlugins.KOTLIN_ANDROID)
+    id(BuildPlugins.KOTLIN_COMPOSE)
+}
+
+android {
+    namespace = BuildConfig.APP_ID
+    compileSdk = BuildConfig.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        applicationId = BuildConfig.APP_ID
+        minSdk = BuildConfig.MIN_SDK_VERSION
+        targetSdk = BuildConfig.TARGET_SDK_VERSION
+        versionCode = ReleaseConfig.VERSION_CODE
+        versionName = ReleaseConfig.VERSION_NAME
+
+        testInstrumentationRunner = TestBuildConfig.TEST_INSTRUMENTATION_RUNNER
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(Dependencies.ANDROIDX_CORE)
+    implementation(Dependencies.ANDROIDX_LIFECYCLE_RUNTIME_KTX)
+    implementation(Dependencies.ANDROIDX_ACTIVITY_COMPOSE)
+    implementation(Dependencies.ANDROIDX_UI)
+    implementation(Dependencies.ANDROIDX_UI_GRAPHICS)
+    implementation(Dependencies.ANDROIDX_UI_TOOLING_PREVIEW)
+    implementation(Dependencies.ANDROIDX_MATERIAL3)
+    implementation(platform(libs.androidx.compose.bom))
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -0,0 +1,13 @@
+object DependenciesVersions {
+
+    const val CORE_KTX = "1.16.0"
+    const val LIFE_CYCLE_RUNTIME_KTX = "2.9.1"
+    const val KOTLIN = "2.0.21"
+    const val JUNIT = "4.13.2"
+    const val JUNIT_VERSION = "1.2.1"
+    const val ESPRESSO_CORE = "3.6.1"
+    const val ACTIVITY_COMPOSE = "1.10.1"
+    const val COMPOSE_UI = "1.8.2"
+    const val MATERIAL3 = ""
+
+}
